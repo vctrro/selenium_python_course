029617ed5647bd3639f80a0530d618099db0472e
Victor Rodionov <vctr.ro@gmail.com>
2022-01-20 18:55:09 +0400

    Initial local env

---
+++ get_method.py
@@ -0,0 +1,32 @@
+import time
+
+# webdriver это и есть набор команд для управления браузером
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+# driver = webdriver.Firefox()
+driver = webdriver.Chrome()
+
+# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+driver.get("https://stepik.org/lesson/25969/step/12")
+time.sleep(5)
+
+# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
+# Ищем поле для ввода текста
+textarea = driver.find_element(By.TAG_NAME, "textarea")
+
+# Напишем текст ответа в найденное поле
+textarea.send_keys("get()")
+time.sleep(5)
+
+# Найдем кнопку, которая отправляет введенное решение
+submit_button = driver.find_element(By.CLASS_NAME, "submit-submission")
+
+# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
+submit_button.click()
+time.sleep(5)
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+driver.quit()
